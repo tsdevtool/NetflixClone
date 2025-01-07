c97f713553afc047e758791bb2d3f3b9a7a084e9
Sieu Nguyen Thanh <122494878+tsdevtool@users.noreply.github.com>
2025-01-07 14:11:58 +0700

    Done setup db

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 import express from "express";
+import { ENV_VARS } from "./config/envVars.js";
+import { connectDB } from "./config/db.js";
 import authRoutes from "./routes/auth.route.js";
+
 const app = express();
+
+const PORT = ENV_VARS.PORT;
 
 app.use("/api/v1/auth", authRoutes);
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
+  connectDB();
 });
