00c5dd4d3757615159810eccce0877de92a2b9ed
Sieu Nguyen Thanh <122494878+tsdevtool@users.noreply.github.com>
2025-01-30 00:07:38 +0700

    done backend fetching TV show API
    trending movies,
    similar movies,
    movie trailers,
    movie details

---
+++ backend/controllers/movie.controller.js
@@ -0,0 +1,80 @@
+import { fetchFromTMDB } from "../services/tmdb.service.js";
+export const getTrendingMovie = async (req, res) => {
+  try {
+    const data = await fetchFromTMDB(
+      "https://api.themoviedb.org/3/trending/movie/day?language=en-US"
+    );
+
+    if (!data || !data.results || data.results.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "No trending movies found",
+      });
+    }
+
+    const randomMovie =
+      data.results[Math.floor(Math.random() * data.results?.length)];
+
+    res.json({ success: true, content: randomMovie });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error in Trending Movies",
+    });
+  }
+};
+
+export const getMovieTrailers = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`
+    );
+    res.json({ success: true, trailers: data.results });
+  } catch (error) {
+    if (error.message.includes("404")) {
+      return res.status(404).send(null);
+    }
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error in Movie Trailers",
+    });
+  }
+};
+
+export const getMovieDetails = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const data = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/movie/${id}?language=en-US`
+    );
+
+    res.status(200).json({ success: true, content: data });
+  } catch (error) {
+    if (error.message.includes("404")) {
+      return res.status(404).send(null);
+    }
+
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error in Movie Details",
+    });
+  }
+};
+
+export const getSimilarMovies = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/movie/${id}/similar?language=en-US&page=1`
+    );
+
+    res.status(200).json({ success: true, similar: data.results });
+  } catch (error) {
+    if (error.message.includes("404")) {
+      return res.status(404).send(null);
+    }
+    return res.status(500).json("Interval Server Error");
+  }
+};
