f0bfeb6756f60492fc2982a20ebd8d329585cd9e
Sieu Nguyen Thanh <122494878+tsdevtool@users.noreply.github.com>
2025-01-31 13:08:27 +0700

    Done search Router
    Search Person
    Search Movie
    Search TV
    Get Search History
    Delete Item From Search History

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import { connectDB } from "./config/db.js";
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
 import tvRoutes from "./routes/tv.route.js";
-
+import searchRoutes from "./routes/search.route.js";
 import { protectRoute } from "./middleware/protectRoute.middleware.js";
 
 const app = express();
@@ -18,6 +18,7 @@ app.use(cookieParser());
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", protectRoute, movieRoutes);
 app.use("/api/v1/tv", protectRoute, tvRoutes);
+app.use("/api/v1/search", protectRoute, searchRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
