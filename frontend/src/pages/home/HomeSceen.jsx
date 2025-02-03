21bb22b6616838c2c308434dc3a59232c4c26e32
Sieu Nguyen Thanh <122494878+tsdevtool@users.noreply.github.com>
2025-02-03 13:41:15 +0700

    Done navbar

--- frontend/src/pages/home/HomeSceen.jsx
+++ frontend/src/pages/home/HomeSceen.jsx
@@ -1,11 +1,29 @@
-import { useAuthStore } from "../../store/authUser";
+import Navbar from "../../components/Navbar";
 
 const HomeSceen = () => {
-  const { logout } = useAuthStore();
   return (
-    <div>
-      <button onClick={logout}>Logout</button>
-    </div>
+    <>
+      <div className="relative h-screen text-white">
+        <Navbar />
+
+        <img
+          src="/extraction.jpg"
+          alt="Hero img"
+          className="absolute top-0 left-0 w-full h-full object-cover -z-50"
+        />
+
+        <div
+          className="absolute top-0 left-0 w-full h-full bg-black/50 -z-50"
+          aria-hidden="true"
+        />
+
+        <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center px-8 md:px-16 lg:px-32">
+          <div className="bg-gradient-to-b from-black via-transparent to-transparent absolute w-full h-full top-0 left-0 -z-10"/>
+          
+
+        </div>
+      </div>
+    </>
   );
 };
 
