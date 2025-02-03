de203fbbcfed453fe353ec3f14d65353db39a97f
Sieu Nguyen Thanh <122494878+tsdevtool@users.noreply.github.com>
2025-02-03 22:12:41 +0700

    Done UI trending information

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { LogOut, Menu, Search } from "lucide-react";
 import { useAuthStore } from "../store/authUser";
+import { useContentStore } from "../store/content";
 const Navbar = () => {
   const [isMobileOpen, setIsMobileOpen] = useState(false);
   const { user, logout } = useAuthStore();
@@ -9,6 +10,8 @@ const Navbar = () => {
   const toggleMobileMenu = () => {
     setIsMobileOpen(!isMobileOpen);
   };
+
+  const { setContentType } = useContentStore();
   return (
     <header className="max-w-6xl mx-auto flex flex-wrap items-center justify-between p-4 h-20">
       <div className="flex items-center gap-10 z-50">
@@ -21,10 +24,18 @@ const Navbar = () => {
         </Link>
         {/* Desktop navbar items */}
         <div className="hidden sm:flex items-center ">
-          <Link to={"/"} className="hover:underline">
+          <Link
+            to={"/"}
+            className="hover:underline"
+            onClick={() => setContentType("movie")}
+          >
             Movies
           </Link>
-          <Link to={"/"} className="hover:underline">
+          <Link
+            to={"/"}
+            className="hover:underline"
+            onClick={() => setContentType("tv")}
+          >
             Tv Shows
           </Link>
           <Link to={"/history"} className="hover:underline">
--- frontend/src/pages/home/HomeSceen.jsx
+++ frontend/src/pages/home/HomeSceen.jsx
@@ -1,6 +1,10 @@
+import { Link } from "react-router-dom";
 import Navbar from "../../components/Navbar";
+import { Info, Play } from "lucide-react";
+import useGetTrendingContent from "../../hooks/useGetTrendingContent";
 
 const HomeSceen = () => {
+  const { trendingContent } = useGetTrendingContent();
   return (
     <>
       <div className="relative h-screen text-white">
@@ -18,9 +22,35 @@ const HomeSceen = () => {
         />
 
         <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center px-8 md:px-16 lg:px-32">
-          <div className="bg-gradient-to-b from-black via-transparent to-transparent absolute w-full h-full top-0 left-0 -z-10"/>
-          
+          <div className="bg-gradient-to-b from-black via-transparent to-transparent absolute w-full h-full top-0 left-0 -z-10" />
+          <div className="max-w-2xl">
+            <h1 className="mt-4 text-6xl font-extrabold text-balance">
+              Extraction
+            </h1>
+            <p className="mt-2 text-lg">2014 | 18+</p>
+            <p className="mt-4 text-lg">
+              Chris Hemsowrth stars in this nonstop action-thriller with
+              Rudhraksh Jaiswal, Randeep Hoodda and Golshifteh Farahani.
+            </p>
+          </div>
 
+          <div className="flex mt-8 ">
+            <Link
+              to={"/watch/123"}
+              className="bg-white hover:bg-white/80 text-black font-bold py-2 px-4 rounded mr-4 flex items-center"
+            >
+              <Play className="size-6  mr-2 fill-black" />
+              Play
+            </Link>
+
+            <Link
+              to={"/watch/123"}
+              className="bg-gray-500/70 hover:bg-gray-500 text-white py-2 px-4 rounded flex items-center"
+            >
+              <Info className="size-6  mr-2 " />
+              More Info
+            </Link>
+          </div>
         </div>
       </div>
     </>
