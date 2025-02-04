47ae3ef6b45bf6cc4f9447a657b6132934854635
Sieu Nguyen Thanh <122494878+tsdevtool@users.noreply.github.com>
2025-02-04 15:20:44 +0700

    Done  built home screen

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,9 @@
+import tailwindScrollBar from "tailwind-scrollbar-hide";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [tailwindScrollBar],
 };
