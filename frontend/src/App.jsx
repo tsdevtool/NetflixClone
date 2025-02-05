5ba49ce3c612b2b5905c6b0ceb7244363702de59
Sieu Nguyen Thanh <122494878+tsdevtool@users.noreply.github.com>
2025-02-05 21:47:55 +0700

    Built search History page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import { useEffect } from "react";
 import { Loader } from "lucide-react";
 import WatchPage from "./pages/WatchPage";
 import SearchPage from "./pages/SearchPage";
+import SearchHistoryPage from "./pages/SearchHistoryPage";
 
 function App() {
   const { user, isCheckingAuth, authCheck } = useAuthStore();
@@ -47,6 +48,10 @@ function App() {
         <Route
           path="/search"
           element={user ? <SearchPage /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/history"
+          element={user ? <SearchHistoryPage /> : <Navigate to={"/login"} />}
         />
       </Routes>
       <Footer />
--- frontend/src/utils/dateFunction.js
+++ frontend/src/utils/dateFunction.js
@@ -5,3 +5,28 @@ export function formatReleaseDate(date) {
     day: "2-digit",
   });
 }
+
+export function formatDate(dateString) {
+  //Create a Date object from the input date string
+  const date = new Date(dateString);
+  const monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dev",
+  ];
+
+  //Extract the month, day and year from the Date object
+  const month = monthNames[date.getUTCMonth()];
+  const day = date.getUTCDate();
+  const year = date.getUTCFullYear();
+
+  return `${month} ${day}, ${year}`;
+}
